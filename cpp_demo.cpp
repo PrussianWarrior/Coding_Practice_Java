1cd73856b5c7cde0fe689ee7fa93e5ec6b6aca8f
HSCTC_29 <prussianvirtues0805@gmail.com>
2018-01-30 00:06:07 +0700

    changed cpp_demo.cpp. Add filename pattern *.out to .gitignore

--- cpp_demo.cpp
+++ cpp_demo.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
+#include<map>
+#include<iomanip>
+using std::setw;
+using std::map;
 using std::cout;
 using std::endl;
+using std::cin;
 
 int main() {
   int arr_1[] = {1,2,3,4,5,6,7,8,9,10,11};
@@ -10,5 +15,25 @@ int main() {
   cout << "sizeof(arr_1) = " << sizeof(arr_1) << endl;
   cout << "sizeof(size_t) = " << sizeof(size_t) << endl;
   cout << "You embarrass your entire extended family with your galactic stupidity" << endl;
+  cout << "Paul Logan is an attention seeking poorly educated Yankee Idiot." << endl;
+
+  int n;
+  map<unsigned, unsigned> unsigned_fre_map_1;
+  while (cin >> n) {
+    unsigned_fre_map_1[n]++;
+  }
+
+  cout << unsigned_fre_map_1.size() << endl;
+  for (map<unsigned, unsigned>::value_type i : unsigned_fre_map_1) {
+    cout << setw(5) << i.first << " - " << setw(5) << i.second << endl;
+  }
+
+  for (auto it = unsigned_fre_map_1.cbegin(); it != unsigned_fre_map_1.cend(); ++it) {
+    cout << setw(5) << it->first << " - " << setw(5) << it->second << endl;
+  }
+
+  for (map<unsigned, unsigned>::const_iterator it = unsigned_fre_map_1.begin(); it != unsigned_fre_map_1.end(); ++it) {
+    cout << setw(5) << it->first << " - " << setw(5) << it->second << endl;
+  }
   return 1;
 }
