6242d19344ed21e72fd7d98e8f01f83b067602d9
HSCTC_29 <prussianvirtues0805@gmail.com>
2018-01-29 23:32:18 +0700

    Changed the file Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -3,5 +3,10 @@ import java.lang.*;
 public class Hello_World {
   public static void main(String[] args) {
     System.out.println("This is just a test");
+    System.out.println("Congratulations, you just met an engineer who had recently studied Kadane algorithm.");
+    System.out.println("It's trivial. And they say that with exactly the right amount of disdain to let you " +
+      "know how trivial the problem is.");
+    System.out.println("It's trivial. Even a child should know the answer to that one. " + 
+      "You disgrace your entired extended family with your galactic stupidity.");
   }
 }
