22488814531f9b04ce0bae2df090524187ff3905
HSCTC_29 <prussianvirtues0805@gmail.com>
2018-01-31 23:28:04 +0700

    added new set up file setup_file.h and replace all the include statements in cpp_demo_2018_01_31.cpp

---
+++ setup_file.h
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::begin;
+using std::end;
